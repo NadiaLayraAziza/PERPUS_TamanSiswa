2122426a5b184d0596e02ee98d67e4d0ff76677f
Shelyca Surrayensih <70507995+ShelycaSurrayensih@users.noreply.github.com>
2021-04-30 22:35:40 +0700

    Layout

---
+++ resources/views/Layouts/SideNav.blade.php
@@ -0,0 +1,63 @@
+<nav class="pcoded-navbar">
+    <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
+    <div class="pcoded-inner-navbar main-menu">
+        <div class="">
+            <div class="main-menu-header">
+                <img class="img-80 img-radius" src="assets/images/avatar-4.jpg" alt="User-Profile-Image">
+                <div class="user-details">
+                    <span id="more-details">John Doe<i class="fa fa-caret-down"></i></span>
+                </div>
+            </div>
+            <div class="main-menu-content">
+                <ul>
+                    <li class="more-details">
+                        <a href="user-profile.html"><i class="ti-user"></i>View Profile</a>
+                        <a href="#!"><i class="ti-settings"></i>Settings</a>
+                        <a href="auth-normal-sign-in.html"><i class="ti-layout-sidebar-left"></i>Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="p-15 p-b-0">
+            <form class="form-material">
+
+        <div class="pcoded-navigation-label">Halaman Admin</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="">
+                <a href="chart-morris.html" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="ti-bar-chart-alt"></i><b>C</b></span>
+                    <span class="pcoded-mtext">Dashboard</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+            <li class="">
+                <a href="map-google.html" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="ti-map-alt"></i><b>M</b></span>
+                    <span class="pcoded-mtext">Data Anggota</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+            <li class="">
+                <a href="map-google.html" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="ti-map-alt"></i><b>M</b></span>
+                    <span class="pcoded-mtext">Data Buku</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+            <li class="">
+                <a href="map-google.html" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="ti-map-alt"></i><b>M</b></span>
+                    <span class="pcoded-mtext">Data Peminjaman</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+            <li class="">
+                <a href="map-google.html" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="ti-map-alt"></i><b>M</b></span>
+                    <span class="pcoded-mtext">Laporan</span>
+                    <span class="pcoded-mcaret"></span>
+                </a>
+            </li>
+        </ul>
+
+</nav>
